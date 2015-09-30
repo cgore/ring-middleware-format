4da920816006e0a5f2cb32ea35e3fe6d961d46ca
Christopher Mark Gore <cgore@cgore.com>
2015-09-30 11:03:57 -0500

    Private clojars build v0.7.5.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject metosin/ring-middleware-format "0.6.0"
+(defproject cgore/ring-middleware-format "0.7.5"
   :description "Ring middleware for parsing parameters and emitting
   responses in various formats (mainly JSON, YAML and Transit out of
   the box)"
-  :url "https://github.com/metosin/ring-middleware-format"
+  :url "https://github.com/cgore/ring-middleware-format"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
@@ -17,7 +17,7 @@
                  [clojure-msgpack "1.1.1"]
                  [com.cognitect/transit-clj "0.8.281"]]
   :plugins [[codox "0.8.13"]]
-  :codox {:src-dir-uri "http://github.com/metosin/ring-middleware-format/blob/master/"
+  :codox {:src-dir-uri "http://github.com/cgore/ring-middleware-format/blob/master/"
           :src-linenum-anchor-prefix "L"
           :defaults {:doc/format :markdown}}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}
